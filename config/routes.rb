c3f06ddd176a6b2440fba81098dbde8fa2d5db7b
tomomi adachi <tomoni1997@gmai.com>
2023-01-12 23:45:28 +0900

    letter_opener修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'blogs#index'
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" 
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
   resources :favorites, only: [:create, :destroy, :index]
