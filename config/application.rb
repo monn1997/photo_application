8851ecf492d81c921570615bc8669a56a4113c11
tomomi adachi <tomoni1997@gmai.com>
2023-01-12 10:47:03 +0900

    辞書機能の実装

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,18 @@ Bundler.require(*Rails.groups)
 
 module PhotoApp
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
 
     # Configuration for the application, engines, and railties goes here.
     #
